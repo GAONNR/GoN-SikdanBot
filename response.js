bfef43314d45722a4846bf4713daeb9a9e5687b3
G40N <gaon0403@gmail.com>
2018-03-28 22:54:41 +0900

    Add two functions: postTest, postAttachments

--- response.js
+++ response.js
@@ -22,48 +22,56 @@ var options = {
   }
 };
 
+function postText(channel, text) {
+  web.chat.postMessage({
+      channel: channel,
+      as_user: false,
+      username: 'Sikdanbot',
+      icon_url: 'https://pbs.twimg.com/profile_images/975638064348327936/26mLY9Qf_400x400.jpg',
+      text: text
+    })
+    .then((res) => {
+      // `res` contains information about the posted message
+      console.log('Message sent: ', res.ts);
+    })
+    .catch(console.error);
+}
+
+function postAttachments(channel, attachments) {
+  web.chat.postMessage({
+      channel: channel,
+      as_user: false,
+      username: 'Sikdanbot',
+      icon_url: 'https://pbs.twimg.com/profile_images/975638064348327936/26mLY9Qf_400x400.jpg',
+      attachments: attachments
+    })
+    .then((res) => {
+      // `res` contains information about the posted message
+      console.log('Message sent: ', res.ts);
+    })
+    .catch(console.error);
+}
+
 module.exports = {
   randomMenu: function(message) {
-    web.chat.postMessage({
-        channel: message.channel,
-        as_user: false,
-        username: 'Sikdanbot',
-        icon_url: 'https://pbs.twimg.com/profile_images/975638064348327936/26mLY9Qf_400x400.jpg',
-        text: `나같으면 ${pickRandom(menus)} 먹는다`
-      })
-      .then((res) => {
-        // `res` contains information about the posted message
-        console.log('Message sent: ', res.ts);
-      })
-      .catch(console.error);
+    postText(message.channel, `나같으면 ${pickRandom(menus)} 먹는다`);
   },
 
   crawlMenu: function(message) {
     rp(options)
       .then(($) => {
         let menu = $($('.menuTb').find('td').get(1)).text().trim();
-        web.chat.postMessage({
-            channel: conversationId,
-            as_user: false,
-            username: 'Sikdanbot',
-            icon_url: 'https://pbs.twimg.com/profile_images/975638064348327936/26mLY9Qf_400x400.jpg',
-            attachments: [{
-              fallback: '오늘의 식단',
-              color: '#2196f3',
-              title: '오늘의 식단',
-              title_link: 'https://bds.bablabs.com/restaurants?campus_id=JEnfpqCUuR',
-              text: menu,
-              ts: unixTime(new Date())
-            }]
-          })
-          .then((res) => {
-            // `res` contains information about the posted message
-            console.log('Message sent: ', res.ts);
-          })
-          .catch(console.error);
+        postAttachments(message.channel, [{
+          fallback: '오늘의 식단',
+          color: '#2196f3',
+          title: '오늘의 식단',
+          title_link: 'https://bds.bablabs.com/restaurants?campus_id=JEnfpqCUuR',
+          text: menu,
+          ts: unixTime(new Date())
+        }]);
       })
       .catch((err) => {
         throw err;
       });
   }
-}
+};
