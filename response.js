62171350a26459687596ac53c19f800c539a6847
G40N <gaon0403@gmail.com>
2018-03-29 11:30:47 +0900

    Random menu is now generated by today random seed

--- response.js
+++ response.js
@@ -9,18 +9,22 @@ const request = require('request');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const unixTime = require('unix-time');
-const pickRandom = require('pick-random');
+const gen = require('random-seed');
 
-const menus = ['마루', '보쌈', '피자', '찜닭', '집돼', '도시락', '학식'];
+const menus = ['마루', '보쌈', '피자', '찜닭', '집돼', '도시락', '학식', '두메', '교수회관'];
 
 const web = new WebClient(token);
 
 var options = {
-  uri: `http://www.kaist.ac.kr/_prog/fodlst/index.php?site_dvs_cd=kr&menu_dvs_cd=050303&dvs_cd=emp&stt_dt=${new Date().toJSON().slice(0,10)}&site_dvs=`,
+  uri: '',
   transform: function(body) {
     return cheerio.load(body);
   }
 };
+
+function getToday() {
+  return new Date().toJSON().slice(0, 10);
+}
 
 function postText(channel, text) {
   web.chat.postMessage({
@@ -54,10 +58,12 @@ function postAttachments(channel, attachments) {
 
 module.exports = {
   randomMenu: function(message) {
-    postText(message.channel, `나같으면 ${pickRandom(menus)} 먹는다`);
+    let today = new Date().toJSON().slice(0, 10);
+    postText(message.channel, `나같으면 ${menus[gen.create(getToday())(menus.length)]} 먹는다`);
   },
 
   crawlMenu: function(message) {
+    options.uri = `http://www.kaist.ac.kr/_prog/fodlst/index.php?site_dvs_cd=kr&menu_dvs_cd=050303&dvs_cd=emp&stt_dt=${getToday()}&site_dvs=`;
     rp(options)
       .then(($) => {
         let menu = $($('.menuTb').find('td').get(1)).text().trim();
